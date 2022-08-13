0cb2696e7e933a1d5129b1dd0a4705b9c1b89e0b
dhruvj7 <dhruvjain076@outlook.com>
2022-08-13 16:36:26 +0530

    recipe structure

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+  recipes: Recipe[] = [ new Recipe("A test recipe","this is a testing recipe","https://th.bing.com/th/id/OIP.aclJvvwAnYlHcv5vAw706AHaLH?pid=ImgDet&rs=1"),
+                        new Recipe ("second test","second testing","https://th.bing.com/th/id/OIP.aclJvvwAnYlHcv5vAw706AHaLH?pid=ImgDet&rs=1")];
   constructor() { }
 
   ngOnInit(): void {
